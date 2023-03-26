94f458e2ab2e154fac79c6b6f332f77eddded85e
Ro <wondaero@nate.com>
2023-03-27 00:55:20 +0900

    효과 변경 및 카드 랜덤 돌림

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -1,11 +1,11 @@
 const effectObj = {
     effect1: {  //파란 애디션
-        tmpl: (num) => {
+        tmpl: (oldNum, newNum, changedNum) => {
             const tmpl = document.createElement('div');
             tmpl.classList.add('num-wrapper');
             tmpl.classList.add('effect1');
             tmpl.innerHTML = `
-                <div class="num">${num}</div>
+                <div class="num">${changedNum}</div>
                 <div class="effect-obj1">
                     <div class="effect-obj1-1" style="box-shadow: 0 0 5px blue, inset 0 0 10px blue;"></div>
                 </div>
@@ -14,16 +14,16 @@ const effectObj = {
         }
     },
     effect2: {  //랜덤컬러 무지개 에디션
-        tmpl: (num) => {
+        tmpl: (oldNum, newNum, changedNum) => {
             const colorArr = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple'];
             const color = colorArr[getRandomNum(0, colorArr.length - 1)];
             
             function getRandomNum(mn, mx){return Math.floor(Math.random() * (mx - mn + 1)) + mn;};
             const tmpl = document.createElement('div');
             tmpl.classList.add('num-wrapper');
-            tmpl.classList.add('effect1');
+            tmpl.classList.add('effect2');
             tmpl.innerHTML = `
-                <div class="num">${num}</div>
+                <div class="num">${changedNum}</div>
                 <div class="effect-obj1">
                     <div class="effect-obj1-1" style="box-shadow: 0 0 5px ${color}, inset 0 0 10px ${color};"></div>
                 </div>
@@ -32,26 +32,52 @@ const effectObj = {
         }
     },
     effect3: { 
-        tmpl: (num) => {
+        tmpl: (oldNum, newNum, changedNum) => {
             const tmpl = document.createElement('div');
             tmpl.classList.add('num-wrapper');
-            tmpl.classList.add('effect2');
+            tmpl.classList.add('effect3');
             tmpl.innerHTML = `
-            <div class="effect-obj1 num">${num}</div>
-            <div class="effect-obj2 num">${num}</div>
+            <div class="effect-obj1 num">${changedNum}</div>
+            <div class="effect-obj2 num">${oldNum}</div>
             `;
             return tmpl;
         }
     },
     effect4: { 
-        tmpl: (num) => {
+        tmpl: (oldNum, newNum, changedNum) => {
             const tmpl = document.createElement('div');
             tmpl.classList.add('num-wrapper');
-            tmpl.classList.add('effect3');
-            tmpl.innerHTML = `<div class="effect-obj1 num">${num}</div>`;
+            tmpl.classList.add('effect4');
+            tmpl.innerHTML = `<div class="effect-obj1 num">${changedNum}</div>`;
             return tmpl;
         }
-    }
+    },
+    effect5: { 
+        tmpl: (oldNum, newNum, changedNum) => {
+            const tmpl = document.createElement('div');
+            tmpl.classList.add('num-wrapper');
+            tmpl.classList.add('effect5');
+            tmpl.innerHTML = `<div class="effect-obj1 num">${changedNum}</div>`;
+            return tmpl;
+        }
+    },
+    effect6: { 
+        tmpl: (oldNum, newNum, changedNum) => {
+            function getRandomNum(mn, mx){return Math.floor(Math.random() * (mx - mn + 1)) + mn;};
+            const plma = [1, -1];
+            const x = getRandomNum(10, 200) * plma[getRandomNum(0, 1)];
+            const y = getRandomNum(10, 200) * plma[getRandomNum(0, 1)];
+            const tmpl = document.createElement('div');
+            tmpl.classList.add('num-wrapper');
+            tmpl.classList.add('effect6');
+            tmpl.innerHTML = `
+                <div class="effect-obj1 num" style="transform: translate(${x}%, ${y}%);">${newNum}</div>
+                <div class="effect-obj2 num">${oldNum}</div>
+                <div class="effect-obj3 num">${changedNum}</div>
+            `;
+            return tmpl;
+        }
+    },
 }
 
 // function getRandomNum(mn, mx){return Math.floor(Math.random() * (mx - mn + 1)) + mn;};
